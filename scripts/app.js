5426bf3bd3829185e4edaeee73997978884fb221
lollykrown <joe_kayu@yahoo.com>
2020-06-17 23:15:22 +0100

    .

--- scripts/app.js
+++ scripts/app.js
@@ -7,8 +7,12 @@ document.querySelector('button').addEventListener("click", ()=>{
   const interestElem = document.getElementById('interest');
   const totalElem = document.getElementById('total') ;
   const duration =  document.getElementById('duration').value;
+  let savingsElem = document.getElementById('savings').value;
+  if (savingsElem === NaN) {
+    savingsElem = 0
+  }
 
-  let savings = parseFloat(document.getElementById('savings').value);
+  let savings = parseFloat(savingsElem);
   interestRatePerDay = 0.1/365;
   console.log(interestRatePerDay)
   let dailyInterest = savings * interestRatePerDay;
@@ -18,8 +22,6 @@ document.querySelector('button').addEventListener("click", ()=>{
 
   let total = (savings + interest).toFixed(2);
   console.log(total)
-  const naira = `&#x20A6`;
-  console.log(naira)
 
   dailyElem.innerText = `Daily Interest: ${dailyInterest.toFixed(2)}`;
   interestElem.innerText = `Interest for ${duration} days: ${interest.toFixed(2)}`;
